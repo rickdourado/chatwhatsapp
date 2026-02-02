9b9a068ab9dc6c4c3d05637c86f369f266c4f627
Patrick Dourado Ribeiro <patrick.ribeiro@gmail.com>
2026-02-02 11:00:02 -0300

    feat: implement vertical full-height mobile simulation for web mode

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,12 @@ function App() {
     const { messages, isTyping, sendMessage } = useChat()
 
     return (
-        <div className="flex flex-col h-screen bg-whatsapp-bg-dark">
-            <ChatHeader />
-            <ChatContainer messages={messages} isTyping={isTyping} />
-            <ChatInput onSendMessage={sendMessage} />
+        <div className="app-container">
+            <div className="flex flex-col h-full bg-whatsapp-bg-dark">
+                <ChatHeader />
+                <ChatContainer messages={messages} isTyping={isTyping} />
+                <ChatInput onSendMessage={sendMessage} />
+            </div>
         </div>
     )
 }
